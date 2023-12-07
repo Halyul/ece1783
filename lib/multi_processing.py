b1ec9e0e06f9f68f97e57479debe5084f0e87cf6
Zifeng Zhu <zzf741523@gmail.com>
2023-12-07 17:26:19 -0500

    fixed exercise 2a

--- lib/multi_processing.py
+++ lib/multi_processing.py
@@ -148,9 +148,13 @@ def block_processing_dispatcher(signal_q: mp.Queue, config: Config) -> None:
             reconstructed_path = config.output_path.reconstructed_folder
             if not frame.is_intraframe:
                 frame.prev = prev_frame
+            if config.params.RCflag == 2:
+                config.params.RCflag = 0
             prev_frame, mv_dump, qtc_block_dump, split_counter, bitcount_per_row, bit_count_per_frame = processing(frame, config.params, q_matrix, reconstructed_path, pool)
+            config.params.RCflag = 2
 
-            if config.params.RCflag == 2 and bit_count_per_frame > 8331 * config.params.qp ** 2 - 135000 * config.params.qp + 570000:
+            bit_count_threshold = 8331 * config.params.qp ** 2 - 135000 * config.params.qp + 560000
+            if config.params.RCflag == 2 and bit_count_per_frame > bit_count_threshold:
                 frame = Frame(counter, height, width, params_i=config.params.i, is_intraframe=True)
                 frame.read_from_file(file)
                 frame.convert_type(np.int16)
